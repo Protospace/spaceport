c5b0a93927d82d9c3cb6c2513d8a41363ad23e3a
Tanner Collin <git@tannercollin.com>
2020-01-06 23:05:19 +0000

    Enable browsable API login

--- apiserver/apiserver/settings.py
+++ apiserver/apiserver/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'apiserver.api',
 ]
 
@@ -139,10 +140,20 @@ if DEBUG:
         'rest_framework.renderers.BrowsableAPIRenderer',
     )
 
+DEFAULT_AUTHENTICATION_CLASSES = (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+
+if DEBUG:
+    DEFAULT_AUTHENTICATION_CLASSES += (
+	    'rest_framework.authentication.SessionAuthentication',
+	)
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES,
+    'DEFAULT_AUTHENTICATION_CLASSES': DEFAULT_AUTHENTICATION_CLASSES,
 }
 
 LOGGING = {
--- apiserver/apiserver/urls.py
+++ apiserver/apiserver/urls.py
@@ -10,4 +10,5 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
