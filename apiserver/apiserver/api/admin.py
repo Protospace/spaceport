c1e682478baee0fd7a0adbca648fdb890a8ac2b7
Tanner Collin <git@tannercollin.com>
2020-01-29 23:42:44 +0000

    Register all api models with admin page

--- apiserver/apiserver/api/admin.py
+++ apiserver/apiserver/api/admin.py
@@ -1,3 +1,10 @@
+from django.apps import apps
 from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
 
-# Register your models here.
+app_models = apps.get_app_config('api').get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
