e14cb2e76257867f1e8e44a3672a142298d387fd
Tanner Collin <git@tannercollin.com>
2020-05-07 05:01:18 +0000

    Tighten filesystem permission

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+# restrict everything
 chown -R spaceport:spaceport .
-chmod -R ug=rwX,o=rX .
+chmod -R ug=rwX,o= .
+
+# allow access to static folders
+chmod o=rX .
+
+chmod o=rX webclient/
+chmod -R o=rX webclient/build/
+
+
+chmod o=rX apiserver/
+
+chmod o=rX apiserver/data/
+chmod -R o=rX apiserver/data/static/
+chmod -R o=rX apiserver/backups/
+
+chmod o=rX apiserver/docs/
+chmod o=rX apiserver/docs/build/
+chmod -R o=rX apiserver/docs/build/html
