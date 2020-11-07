e8198f7b2abe84e48d98ec260ee16122b9c98b26
Tanner Collin <git@tannercollin.com>
2020-11-07 21:27:10 +0000

    Filter logging /lockout/

--- apiserver/apiserver/filters.py
+++ apiserver/apiserver/filters.py
@@ -8,3 +8,10 @@ class IgnoreStats(logging.Filter):
             return False
         else:
             return True
+
+class IgnoreLockout(logging.Filter):
+    def filter(self, record):
+        if 'GET /lockout/' in record.msg:
+            return False
+        else:
+            return True
