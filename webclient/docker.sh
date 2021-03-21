cfe4cb338295eeeef65137bc841531b4b82d1ff8
Elijah Lucian <elijahlucian@gmail.com>
2021-03-20 18:59:13 -0700

    👻

--- webclient/docker.sh
+++ webclient/docker.sh
@@ -1,4 +1,4 @@
 docker build . -t spaceport-client
-docker run -v $PWD:/usr/src/app spaceport-client
+docker run -it -p 3000:3000 -v $PWD/src:/usr/src/app/src spaceport-client
 # npm install
 # npm run start
