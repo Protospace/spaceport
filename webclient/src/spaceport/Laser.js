955f2e4463e17d1ba66d6243e7d1f10638ff43f8
Elijah Lucian <elijahlucian@gmail.com>
2021-03-20 20:00:20 -0700

    🩸🐢🔫🐇

--- webclient/src/spaceport/Laser.js
+++ webclient/src/spaceport/Laser.js
@@ -2,22 +2,25 @@ import * as THREE from 'three';
 
 const LASER_SIZE = 0.5;
 
+const geometry = new THREE.BoxGeometry(
+	LASER_SIZE * 0.03,
+	LASER_SIZE * 0.03,
+	LASER_SIZE
+);
+
+const material = new THREE.MeshStandardMaterial(0xffffff, {
+	flatShading: true,
+});
+
 export class Laser {
 	constructor(ship) {
 		const position = new THREE.Vector3();
 		this.direction = ship.direction;
 
-		ship.mesh.getWorldPosition(position)
+		ship.mesh.getWorldPosition(position);
 
-		const shipGeo = new THREE.BoxGeometry(
-			LASER_SIZE * 0.03,
-			LASER_SIZE * 0.03,
-			LASER_SIZE
-		);
-		this.mesh = new THREE.Mesh(
-			shipGeo,
-			new THREE.MeshStandardMaterial(this.direction > 0 ? 0xff0000 : 0x0000ff, { flatShading: true })
-		);
+		const shipGeo = geometry;
+		this.mesh = new THREE.Mesh(shipGeo, material);
 
 		this.mesh.material.color.set(
 			new THREE.Color(`hsl(${ship.direction > 0 ? 0 : 180},100%,70%)`)
--- webclient/src/spaceport/Ship.js
+++ webclient/src/spaceport/Ship.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three';
 
 const SHIP_SIZE = 1;
+const geometry = new THREE.BoxGeometry(
+	SHIP_SIZE * 0.1,
+	SHIP_SIZE * 0.1,
+	SHIP_SIZE
+);
+const material = new THREE.MeshStandardMaterial(0xffffff, {
+	flatShading: true,
+});
 
 export class Ship {
 	constructor(direction) {
@@ -11,15 +19,8 @@ export class Ship {
 		this.flyIn = true;
 		this.firing = false;
 
-		const shipGeo = new THREE.BoxGeometry(
-			SHIP_SIZE * 0.1,
-			SHIP_SIZE * 0.1,
-			SHIP_SIZE
-		);
-		this.mesh = new THREE.Mesh(
-			shipGeo,
-			new THREE.MeshStandardMaterial(this.direction > 0 ? 0xff0000 : 0x0000ff, { flatShading: true })
-		);
+		const shipGeo = geometry;
+		this.mesh = new THREE.Mesh(shipGeo, material);
 		this.y = (Math.random() - 0.5) * 2;
 		this.x = (Math.random() - 0.5) * 2;
 
@@ -28,7 +29,7 @@ export class Ship {
 		);
 		this.mesh.position.y = this.y;
 		this.mesh.position.x = this.x;
-		this.mesh.position.z = (-475/4 - 6) * this.direction; //+ Math.random()
+		this.mesh.position.z = (-475 / 4 - 6) * this.direction; //+ Math.random()
 	}
 
 	update({ deltaTime }) {
@@ -52,10 +53,10 @@ export class Ship {
 		this.life -= deltaTime;
 
 		if (!this.flyIn) {
-			const xs = Math.sin(this.life*0.5 + this.x);
+			const xs = Math.sin(this.life * 0.5 + this.x);
 			this.mesh.position.y = this.y + Math.sin(this.life + this.y) * 0.08;
 			this.mesh.position.x = this.x + xs * 0.08;
-			this.mesh.rotation.y = xs*0.25;
+			this.mesh.rotation.y = xs * 0.25;
 			this.mesh.position.z += 0.01 * this.direction;
 			this.nextShot -= deltaTime;
 			if (this.nextShot <= 0) {
@@ -71,7 +72,7 @@ export class Ship {
 			this.firing = false;
 		}
 
-		if (Math.abs(this.mesh.position.z > 475/2)) {
+		if (Math.abs(this.mesh.position.z > 475 / 2)) {
 			this.kill = true;
 		}
 	}
