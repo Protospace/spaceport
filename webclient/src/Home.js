0b27a8bd5de02c98443d602a2fd4734c7bb48a37
Tanner Collin <git@tannercollin.com>
2021-09-23 21:13:36 +0000

    Grammar

--- webclient/src/Home.js
+++ webclient/src/Home.js
@@ -30,9 +30,9 @@ function MemberInfo(props) {
 					<Header size='large'>{member.preferred_name} {member.last_name}</Header>
 
 					{member.is_allowed_entry ?
-						<p>You are allowed entry to Protospace ✅</p>
+						<p>You are allowed entry into Protospace ✅</p>
 					:
-						<p>You are not allowed entry to Protospace ❌</p>
+						<p>You are not allowed entry into Protospace ❌</p>
 					}
 
 					<BasicTable>
