42b878abbf9a82472ffe359d8ac22da03578bea8
Tanner Collin <git@tannercollin.com>
2022-04-06 23:17:39 +0000

    Bring back the static files

--- apiserver/apiserver/settings.py
+++ apiserver/apiserver/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.sites',
     'rest_framework',
     'rest_framework.authtoken',
@@ -187,6 +188,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 if DEBUG:
+    STATIC_URL = 'devstatic/'
     MEDIA_URL = 'static/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'data/static')
 else:
