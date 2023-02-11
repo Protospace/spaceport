d8e72a81681e65e962963634bfbf07b001f16bb2
Tanner Collin <git@tannercollin.com>
2023-02-11 21:20:04 +0000

    Export num students and attended in class report

--- apiserver/scripts/export_class_report.py
+++ apiserver/scripts/export_class_report.py
@@ -8,7 +8,7 @@ from apiserver.api import models
 sessions = models.Session.objects.filter(datetime__gte='2021-01-01')
 
 with open('output.csv', 'w', newline='') as csvfile:
-    fields = ['date', 'name', 'num_students']
+    fields = ['date', 'name', 'num_students','attended']
     writer = csv.DictWriter(csvfile, fieldnames=fields)
 
     writer.writeheader()
@@ -17,6 +17,7 @@ with open('output.csv', 'w', newline='') as csvfile:
         writer.writerow(dict(
             date=s.datetime.date(),
             name=s.course.name,
-            num_students=s.students.filter(attendance_status='Attended').count(),
+            num_students=s.students.count(),
+            attended=s.students.filter(attendance_status='Attended').count(),
         ))
 
