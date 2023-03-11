e00bea9faa148d38ed2c48353d23114bdbb9a7e5
Tanner Collin <git@tannercollin.com>
2023-03-11 01:47:08 +0000

    Don't log hosting high scores route

--- apiserver/apiserver/api/throttles.py
+++ apiserver/apiserver/api/throttles.py
@@ -24,7 +24,7 @@ class LoggingThrottle(throttling.BaseThrottle):
             return True
         elif path == '/sessions/' and user == None:
             return True
-        elif path in ['/pinball/high_scores/', '/protocoin/printer_balance/']:
+        elif path in ['/pinball/high_scores/', '/protocoin/printer_balance/', '/hosting/high_scores/']:
             return True
 
         if request.data:
