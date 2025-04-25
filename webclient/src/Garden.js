344be7f27eb24cd648ac8454a91f18fa4f8e6e49
Tanner Collin <git@tannercollin.com>
2025-04-25 20:16:07 +0000

    Add timestamp to garden large pic request

--- webclient/src/Garden.js
+++ webclient/src/Garden.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment-timezone';
 import { Container, Header, Image } from 'semantic-ui-react';
 import { staticUrl } from './utils.js';
 
@@ -7,7 +8,7 @@ export function Garden(props) {
 		<Container>
 			<Header size='large'>Protogarden</Header>
 
-			<Image src={staticUrl + '/garden-large.jpg'} />
+			<Image src={staticUrl + '/garden-large.jpg?' + moment().unix()} />
 		</Container>
 	);
 };
