4fd3ce22615b4caceda78da673a88d3f48698f2b
Tanner Collin <git@tannercollin.com>
2025-11-03 01:46:56 +0000

    Force Saturnalia class page to dark mode

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -45,6 +45,7 @@ function App() {
 	const history = useHistory();
 
 	useEffect(() => {
+		// note: theme also gets set below in the history.location useEffect
 		document.body.className = isDark ? 'dark' : '';
 		console.log('theme to:', document.body.className || 'light');
 	}, []);
@@ -83,6 +84,13 @@ function App() {
 	}
 
 	useEffect(() => {
+		if (history.location.pathname === '/classes/14640') {
+			// force Saturnalia class page to dark
+			document.body.className = 'dark';
+		} else {
+			document.body.className = isDark ? 'dark' : '';
+		}
+
 		if (user) {
 			const data = {
 				id: user.member.id,
